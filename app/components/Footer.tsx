0e259f611f820152beb80aab737cc4be38362788
kehinde-am <kennyamusa99@gmail.com>
2024-07-08 21:56:43 +0100

    style: make mobile responsive

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -10,8 +10,8 @@ const Footer: React.FC = () => {
       <div className="container mx-auto px-4">
         <div className="flex flex-wrap justify-between">
           <div className="w-full md:w-1/3 mb-6">
-            <h2 className="text-xl font-normal"style={{ fontFamily: 'Goldman' }}>HilltTech.</h2>
-            <p>We are a major distributor<br></br> of Oppo phones and<br></br>accessories in Nigeria.</p>
+            <h2 className="text-xl font-normal" style={{ fontFamily: 'Goldman' }}>HilltTech.</h2>
+            <p>We are a major distributor<br /> of Oppo phones and<br />accessories in Nigeria.</p>
           </div>
           <div className="w-full md:w-1/3 mb-6">
             <h2 className="text-lg font-bold">Shopping</h2>
@@ -25,31 +25,31 @@ const Footer: React.FC = () => {
           <div className="w-full md:w-1/3 mb-7">
             <h2 className="text-lg font-bold mb-4">Contact Us</h2>
             <ul>
-              <li className="flex items-center mb-5" style={{ width: '273px', height: '40px' }}>
-                <FontAwesomeIcon icon={faPhone} className="mr-2" style={{ width: '40px', height: '40px' }} /> +234 902 4567 100
+              <li className="flex items-center mb-5">
+                <FontAwesomeIcon icon={faPhone} className="mr-2" /> +234 902 4567 100
               </li>
-              <li className="flex items-center mb-5" style={{ width: '315px', height: '40px' }}>
-                <FontAwesomeIcon icon={faEnvelope} className="mr-2" style={{ width: '40px', height: '40px' }} /> hillttech@hilltinc.com
+              <li className="flex items-center mb-5">
+                <FontAwesomeIcon icon={faEnvelope} className="mr-2" /> hillttech@hilltinc.com
               </li>
-              <li className="flex items-center mb-5" style={{ width: '417px', height: '40px' }}>
-                <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" style={{ width: '40px', height: '40px' }} /> No 2, Bola Street, Ikeja, Lagos
+              <li className="flex items-center mb-5">
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" /> No 2, Bola Street, Ikeja, Lagos
               </li>
             </ul>
             <div className="flex mt-4">
               <Link href="#" className="mr-4">
-                <FontAwesomeIcon icon={faInstagram} style={{ width: '40px', height: '40px' }} />
+                <FontAwesomeIcon icon={faInstagram} />
               </Link>
               <Link href="#" className="mr-4">
-                <FontAwesomeIcon icon={faXTwitter} style={{ width: '40px', height: '40px' }} />
+                <FontAwesomeIcon icon={faXTwitter} />
               </Link>
               <Link href="#">
-                <FontAwesomeIcon icon={faWhatsapp} style={{ width: '40px', height: '40px' }} />
+                <FontAwesomeIcon icon={faWhatsapp} />
               </Link>
             </div>
           </div>
         </div>
-        <div className="mt-6 flex justify-between items-center text-sm">
-          <p>© 2024 HilltTech Inc. All rights reserved.</p>
+        <div className="mt-6 flex flex-col md:flex-row justify-between items-center text-sm">
+          <p className="mb-2 md:mb-0">© 2024 HilltTech Inc. All rights reserved.</p>
           <p>
             <Link href="/privacy-policy" className="mr-4">Privacy Policy</Link>
             <Link href="/terms-and-conditions">Terms and Conditions</Link>
