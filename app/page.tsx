c6aef8813932393f9b6ae9228b675681c9577d59
kehinde-am <kennyamusa99@gmail.com>
2024-07-09 12:36:34 +0100

    refactor: import products list page as default page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
+import Products from './components/ProductList';
 
 const HomePage: React.FC = () => {
   return (
     <div>
-    <Header/>
+    <Products/>
       </div>
   );
 }
